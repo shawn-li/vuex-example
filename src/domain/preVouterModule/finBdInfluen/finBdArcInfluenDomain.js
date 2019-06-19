3c93f7571be2e182e726190b0bf492b6f5440315
unknown <707361845@qq.com>
2019-06-19 15:23:08 +0800

    first commit

--- src/domain/preVouterModule/finBdInfluen/finBdArcInfluenDomain.js
+++
@@ -1,7 +0,0 @@
-let finBdArcInfluenDomain = [
-  {id: "arcInfluenCode", label: '自定义影响因素档案编码', required: true, width:'*' },
-  {id: "arcInfluenName", label: '自定义影响因素档案名称', required: true, width:'*' },
-]
-export default finBdArcInfluenDomain;
-
-
